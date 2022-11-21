77df04b8c3d6793e142818a6db9f250970ca0ff9
willson.wang <drdangran@gmail.com>
2022-11-21 20:44:00 +0800

    1. init proj

---
+++ src/main/java/tiny/lang/ee/JpaWorksApplication.java
@@ -0,0 +1,12 @@
+package tiny.lang.ee;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JpaWorksApplication {
+    
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(JpaWorksApplication.class, args);
+    }
+}
